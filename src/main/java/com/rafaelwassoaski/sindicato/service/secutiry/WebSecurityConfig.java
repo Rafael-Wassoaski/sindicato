d39e6dcef2d60f0e76c6a066700565e63a336893
rafael.wassoaski <meireles200@hotmail.com>
2024-01-18 20:57:55 -0300

    SDN-7: Adicionando validação de form de cadastro

--- src/main/java/com/rafaelwassoaski/sindicato/service/secutiry/WebSecurityConfig.java
+++ src/main/java/com/rafaelwassoaski/sindicato/service/secutiry/WebSecurityConfig.java
@@ -31,6 +31,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/", "/home", "/js/**", "/css/**")
+                .permitAll()
+
                 .antMatchers("/users/login")
                 .permitAll()
 
---
+++ src/main/resources/static/js/userCreation.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+  $('#CPF').mask('000.000.000-00');
+});
+
+function validate(){
+    console.log("aaaaa");
+
+    let cpf = document.getElementById("CPF");
+    let password = document.getElementById("password");
+    let confirmPassword = document.getElementById("confirm-password");
+
+    let isCpsValid = validateCPF(cpf);
+    let isPasswordValid = validatePassword(password, confirmPassword);
+
+
+    if(!isCpsValid){
+        alert("CPF inválido");
+        return;
+    }
+
+     if(!isPasswordValid){
+        let mensagemSenha = "Senha inválida";
+        if(validatePasswordSize(password)){
+                mensagemSenha += ", a senha deve ter no mínimo 8 caracteres";
+            }
+
+        alert(mensagemSenha);
+        return;
+    }
+
+    return true;
+}
+
+function validateCPF(cpf) {
+  cpf = cpf.replace(/\D/g, '');
+  if (cpf.length !== 11) {
+    return false;
+  }
+
+  if (/^(\d)\1{10}$/.test(cpf)) {
+    return false;
+  }
+
+  let soma = 0;
+  for (let i = 0; i < 9; i++) {
+    soma += parseInt(cpf.charAt(i)) * (10 - i);
+  }
+  let primeiroDigito = 11 - (soma % 11);
+  primeiroDigito = primeiroDigito > 9 ? 0 : primeiroDigito;
+
+  if (parseInt(cpf.charAt(9)) !== primeiroDigito) {
+    return false;
+  }
+
+  soma = 0;
+  for (let i = 0; i < 10; i++) {
+    soma += parseInt(cpf.charAt(i)) * (11 - i);
+  }
+  let segundoDigito = 11 - (soma % 11);
+  segundoDigito = segundoDigito > 9 ? 0 : segundoDigito;
+
+  if (parseInt(cpf.charAt(10)) !== segundoDigito) {
+    return false;
+  }
+  return true;
+}
+
+
+function validatePassword(password, confirmPassword){
+
+    if(!validatePasswordSize(password)){
+        return false;
+    }
+
+    if(password !== confirmPassword){
+        return false;
+    }
+
+    return true;
+}
+
+function validatePasswordSize(password){
+    if(password == null || password.length < 8){
+            return false;
+    }
+
+    return true;
+}
